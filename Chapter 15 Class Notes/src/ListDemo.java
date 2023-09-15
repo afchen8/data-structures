15b5aa14f67b8957ce96fa6b537df9e8499c2a2e
Athena Chen <afchen@stu.naperville203.org>
2023-09-15 12:41:57 -0500

    one more example of an enhanced for loop

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -48,7 +48,55 @@ public class ListDemo
         iterator.remove(); // will remove last called element aka noah kahan
 
         System.out.println(staff);
-        System.out.println("Expected: lizzy mcalpine, jeremy zucker, olivia rodrigo, taylor swift, chelsea cutler");
+        System.out.println("Expected: [lizzy mcalpine, jeremy zucker, olivia rodrigo, taylor swift, chelsea cutler]");
+
+        /* set method updates element returned by last call to next or previous */
+        iterator.previous(); // LJOT|C
+        iterator.set("henry moodie"); // LJOT|M
+
+        /* hasNext method returns true if set has another element
+         * used in condition of a while loop
+         */
+        iterator = staff.listIterator(); // retsets iterator to |LJOTM
+        while(iterator.hasNext());
+        {
+            String = iterator.next();
+            if(n.equals("jeremy zucker"))
+            { .. 
+                iterator.remove();
+            }
+        }
+
+        /* enhanced for loops work with linked lists */
+        for(String n: staff)
+        {
+            System.out.println(n + " " );
+        }
+        System.out.println("Expected: [lizzy mcalpine olivia rodrigo taylor swift henry moodie]");
+
+        /* ConcurrentModificationException
+         * can't modify a linked list when using an iterator
+         * unless you use the iterator to do so
+         * but this just removes stuff
+         */
+        iterator = staff.listIterator(); // resets
+        while(iterator.hasNext())
+        {
+            String n = iterator.next();
+            if (n.equals("henry moodie")
+                // staff.remove("Diana");
+        }
+
+
+        // 09.15.23
+
+
+        /*an enhanced for lop AUTOMATICALLY creates an iterator (that's why we don't need to make one) */
+        for(String n: staff)
+            if(n.equals("henry moodie"))
+            {
+                staff.add("mina okabe")
+            }
 
 
 
