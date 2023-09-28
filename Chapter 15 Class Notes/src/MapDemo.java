31cf644747ba9761899c76973670654ded646578
Athena Chen <111458638+afchen8@users.noreply.github.com>
2023-09-28 13:09:05 -0500

    just updating everything on this computer

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,23 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        // the Map interface is generic:
+        // the first type is the type of the key
+        // the second type is the type of the values
+        Map<String, Color> favColors = new HashMap<>();
+
+        // use put to put things into the map, add key and value
+        favColors.put("Hyder", Color.RED);
+        favColors.put("Athena", Color.MAGENTA);
+        favColors.put("Bavya", Color.BLUE);
+
+        // two different keys can have the same value
+        // same color of red
+        favColors.put("Mitchell", Color.RED);
+
+        // BUT the same key can't have two different values
+        favColors.put("Athena", Color.ORANGE);
+
+        System.out.println(favColors.get("Athena"));
     }
 }
