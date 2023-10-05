efb84b4b56ed60bc0d5b68f08c4ca050bee86c33
Athena Chen <111458638+afchen8@users.noreply.github.com>
2023-10-05 13:29:55 -0500

    class notes copied

--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,26 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        //create a print queue of strings (using a linked list, which is a queue!)
+        Queue<String> jobs = new LinkedList<>();
+        jobs.add("Joe: Expense Report 2023");
+        jobs.add("Cathy: Top Secret Document #5");
+
+        System.out.println("Printing... " + jobs.remove());//first in, first out, so joe will be printed first
+        // prints one at a time
+
+        jobs.add("Cathy: Really Top Secret Document #6");
+        jobs.add("Joe: Grocery List");
+        jobs.add("Cathy: Can I Get Fired For This?");
+
+        System.out.println("Printing... " + jobs.remove());//prints cathy's top secret document #5
+
+        jobs.add("Boss: Cathy's Termination Letter");
+        
+        //howw to print the rest of the jobs in the queue
+        while(!jobs.isEmpty()) // jobs is not empty
+        {
+            System.out.println("Pringint... " + jobs.remove());
+        }
     }
 }
--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -1,5 +1,10 @@
 import java.util.Stack;
 
+
+/*
+ * notes:
+ *      most common use of a stack is to undo things
+ */
 /**
  * This program simulates an undo stack. Note that operations
  * must be undone in the opposite order in which they are first
@@ -9,5 +14,27 @@ public class StackDemo
 {
     public static void main(String[] args)
     {
+        Stack<String>commands = new Stack<>();//don't need to specify size of stack when instantiating
+
+        //push commands onto the top of the stack
+        commands.push("Insert: 'Hello'");
+        commands.push("Insert: ','");
+        commands.push("Insert: ' '");
+        commands.push("Insert: 'World'");
+        commands.push("Insert: '?'");
+        commands.push("Delete: '?'");
+        commands.push("Insert: '!'");
+
+        //print the stack; top of stack is on the far right
+        System.out.println(commands);
+
+        //simulate the user pressing undo button four times
+        for(int i = 0; i < 4; i++)
+        {
+            String command = commands.pop();
+            System.out.println("Undo: " + command);
+        }
+            
+
     }
 }
